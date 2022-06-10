594629c2e801d2ec219c96ac27c206244981350e
JiaXiao243 <jiaxiao243@126.com>
2022-06-10 06:41:55 +0000

    update bat

--- PaddleSpeech/PaddleSpeech_windows_CI_dev.bat
+++ PaddleSpeech/PaddleSpeech_windows_CI_dev.bat
@@ -35,8 +35,56 @@ call :printFun vector_score_offline
 rem text
 paddlespeech_client text --server_ip 127.0.0.1 --port 8090 --input "我认为跑步最重要的就是给我带来了身体健康"
 call :printFun text_offline
+call :killFun
+call :killFun
 
-taskkill /f /im paddlespeech_server*
+rem  online_tts
+cd ../streaming_tts_server
+rem  http
+start paddlespeech_server start --config_file ./conf/tts_online_application.yaml 2>&1 &
+timeout /nobreak /t 30
+
+paddlespeech_client tts_online --server_ip 127.0.0.1 --port 8092 --protocol http --input "�~B�好�~L欢�~N使�~T��~Y�度�~^桨语�~_��~P~H�~H~P�~\~M�~J��~@~B" --output output..
+wav
+call :printFun tts_online_http
+call :killFun
+
+rem websocket
+set sed="C:\Program Files\Git\usr\bin\sed.exe"
+%sed% -i s/"http"/"websocket"/g ./conf/tts_online_application.yaml
+rem sed -i "s/device: 'cpu'/device: 'gpu:5'/g" ./conf/tts_online_application.yaml
+start paddlespeech_server start --config_file ./conf/tts_online_application.yaml 2>&1 &
+timeout /nobreak /t 30
+
+paddlespeech_client tts_online --server_ip 127.0.0.1 --port 8092 --protocol websocket --input "�~B�好�~L欢�~N使�~T��~Y�度�~^桨语�~_��~P~H�~H~P�~\~M�~J��~@~B" --output ouu
+tput.wav
+call :printFun tts_online_websockert
+call :killFun
+
+rem online_asr
+cd ../streaming_asr_server
+wget -c https://paddlespeech.bj.bcebos.com/PaddleAudio/zh.wav https://paddlespeech.bj.bcebos.com/PaddleAudio/en.wav
+
+rem sed -i "s/device: 'cpu' /device: 'gpu:5'/g"  ./conf/ws_conformer_wenetspeech_application.yaml
+start paddlespeech_server start --config_file ./conf/ws_conformer_wenetspeech_application.yaml 2>&1 &
+timeout /nobreak /t 30
+
+paddlespeech_client asr_online --server_ip 127.0.0.1 --port 8090 --input ./zh.wav
+call :printFun asr_online_websockert
+call :killFun
+
+:printFun
+if not %errorlevel% == 0 (
+        echo  %~1 predict failed!
+) else (
+        echo  %~1 predict successfully!
+)
+EXIT /B 0
+
+:killFun
+taskkill /f /im /T paddlespeech_server*
+rem taskkill /f /im python.exe
+EXIT /B 0
 
 :printFun
 if not %errorlevel% == 0 (
